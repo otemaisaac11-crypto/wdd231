80c2daeca394564b0a4892ebabc8831620990621
Otema Isaac <otemaisaac11@gmail.com>
2026-07-21 00:09:37 +0300

    ES modules

---
+++ week03/scripts/modules.mjs
@@ -0,0 +1,24 @@
+// 1. Import your default and named modules at the very top
+import byuiCourse from './course.mjs';
+import { setSectionSelection } from './sections.mjs';
+import { setTitle, renderSections } from './output.mjs';
+
+// 2. Build your event listeners and manually trigger UI updates
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum);
+  renderSections(byuiCourse.sections); // Re-renders the updated sections list
+});
+
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum, false);
+  renderSections(byuiCourse.sections); // Re-renders the updated sections list
+});
+
+// 3. Initialize the page data on load
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
+
+({ setSectionSelection }, { setTitle, renderSections })
